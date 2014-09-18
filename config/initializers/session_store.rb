9d266f5370faf236cf97bc4e4bc7e886f08b72d4
Eric Kelly <heroiceric@gmail.com>
2014-09-18 09:56:55 -0400

    Update rails to v 4.1.6

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Muenster::Application.config.session_store :cookie_store, key: '_muenster_session'
+Rails.application.config.session_store :cookie_store, key: '_muenster_session'
